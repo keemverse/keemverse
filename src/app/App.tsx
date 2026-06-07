d3ccd27d4d373885972b6ce0280ca9afaa3d0f55
Keemverse <akeemtajudeen322@gmail.com>
2026-06-07 12:06:57 +0100

    Fix routing and navigation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import { MemoryRouter, Routes, Route, Navigate } from 'react-router';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import { NavBar } from './components/NavBar';
 import { FashionPage } from './pages/FashionPage';
 import { DigitalCraftPage } from './pages/DigitalCraftPage';
 
 export default function App() {
   return (
-    <MemoryRouter initialEntries={['/fashion']}>
-      <div className="min-h-screen" style={{ backgroundColor: '#F5F2EA' }}>
+    <BrowserRouter>
+      <div
+        className="min-h-screen"
+        style={{ backgroundColor: '#F5F2EA' }}
+      >
         <NavBar />
+
         <Routes>
           <Route path="/" element={<Navigate to="/fashion" replace />} />
           <Route path="/fashion" element={<FashionPage />} />
           <Route path="/digital-craft" element={<DigitalCraftPage />} />
         </Routes>
       </div>
-    </MemoryRouter>
+    </BrowserRouter>
   );
 }
